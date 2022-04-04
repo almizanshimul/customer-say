b53367c69938e9393a9f3442df3bc794fe1181aa
Al Mizan <67356360+almizanshimul@users.noreply.github.com>
2022-04-04 22:44:51 +0600

    hero banner added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import './Home.css'
+import './Home.css';
+import tesla from "../../images/tesla.png";
+
 
 const Home = () => {
     return (
         <div className='hero-container'>
            <div className="row">
                <div className="col">
-                   
+                   <div>
+                       <h1>Tesla Model 3</h1>
+                       <p>What you’re looking at is the reason why a lot of car manufacturers are now scrambling to bring out as many electric vehicles (EVs) as they can before government legislation brings the hammer down on fossil-fuel powered vehicles.</p>
+                       <button>Click Here</button>
+                   </div>
                </div>
-               <div className="col"></div>
+               <div className="col">
+                       <img src={tesla} alt="" />
+               </div>
            </div>
         </div>
     );
