1195172cf2194606356211867ef6a01f2c132735
Al Mizan <67356360+almizanshimul@users.noreply.github.com>
2022-04-04 23:48:29 +0600

    404 page design

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>Not Found</h2>
+        <div className='notfound'>
+            
         </div>
     );
 };
